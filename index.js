111cdbca6142152d8751ab471c31280cf7ce1691
Pietro Klein <117283920+pietrokp@users.noreply.github.com>
2022-12-15 22:23:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+// DOM
+// Document Object Model
+// Event-Driven
+// callback function (ex.: função passada como argumento)
+// type module - EcmaScript 2015 ES6 Modules
+
+import Controls from "./controls.js"
+import Timer from "./timer.js"
+import Sound from "./sounds.js"
+import {
+    buttonPause,
+    buttonPlay,
+    buttonSet,
+    buttonSoundOff,
+    buttonSoundOn,
+    buttonStop,
+    minutesDisplay,
+    secondsDisplay
+} from "./elements.js"
+
+const controls = Controls({
+    buttonPlay,
+    buttonPause,
+    buttonSet,
+    buttonStop
+})
+
+const timer = Timer({
+    minutesDisplay,
+    secondsDisplay,
+    resetControls: controls.reset
+})
+
+const sound = Sound()
+
+buttonPlay.addEventListener('click', function() {
+    controls.play()
+    timer.countdown()
+    sound.pressButton()
+})
+
+buttonPause.addEventListener('click', function() {
+    controls.pause()
+    timer.hold()
+    sound.pressButton()
+})
+
+buttonStop.addEventListener('click', function() {
+    controls.reset()
+    timer.reset()
+    sound.pressButton()
+})
+
+buttonSoundOff.addEventListener('click', function() {
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
+    sound.bgAudio.play()
+})
+
+buttonSoundOn.addEventListener('click', function() {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+    sound.bgAudio.pause()
+})
+
+buttonSet.addEventListener('click', function() {
+    let newMinutes = controls.getMinutes()
+
+    if (!newMinutes) {
+        timer.reset()
+        return
+    }
+
+    timer.updateDisplay(newMinutes, 0)
+    timer.updateMinutes(newMinutes)
+})
